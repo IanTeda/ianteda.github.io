00287c96443155405b5d20961ffe205ebdca5c67
Ian Teda <ian@teda.id.au>
2016-01-27 14:52:27 +1100

    Social icons in about

--- gulp/tasks/browsersync.js
+++ gulp/tasks/browsersync.js
@@ -34,6 +34,6 @@ gulp.task('browsersync', function() {
   gulp.watch(['src/**/*.md', 'src/**/*.html', 'src/**/*.yml'], gulp.series('jekyll', reload));
   gulp.watch(['src/**/*.xml', 'src/**/*.txt'], gulp.series('jekyll', reload));
   gulp.watch('src/assets/scripts/**/*.js', gulp.series('scripts', reload));
-  gulp.watch(['src/assets/scss/**/*.scss', 'src/assets/styles/**/*.css'], gulp.series('styles', reload));
+  gulp.watch(['src/assets/sass/**/*.scss', 'src/assets/styles/**/*.css'], gulp.series('styles', reload));
   gulp.watch('src/assets/images/**/*', gulp.series('images', reload));
 });
